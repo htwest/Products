4b972d8b355ba62146da6390d343b01ffd8c9fb1
Hayden West <haydenw7@gmail.com>
2021-03-19 17:47:09 -0700

    impilented test ETL

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config.js');
+const sampleTransformer = require('./Transformers/sampleTransformer.js');
 
 const app = express();
 app.use(express.json());
+
+
+// IMPORT ROUTES
+const testRoute = require('./Routes/test');
+app.use('/test', testRoute);
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('works')
 })
 
+
+// CONNECT TO DB
+mongoose.connect(config.MONGO, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("We're connected to the server!")
+});
+
+
 // SERVER
 app.listen(3000);
+
+// DATA IMPORT AND TRANSFORMATION
+// sampleTransformer();
