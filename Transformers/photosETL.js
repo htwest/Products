bd9caef4364f49e3ece5c698b78e66515be58fea
Hayden West <haydenw7@gmail.com>
2021-03-23 14:11:37 -0700

    attempted fullstyle schema

--- Transformers/photosETL.js
+++ Transformers/photosETL.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const config = require('../config.js');
 const { Readable } = require('stream');
-const csv = require('csv-parser');
+const split = require('split');
 
 const Photo = require('../Models/Photo.model');
 
@@ -22,77 +22,87 @@ let fragment;
   await new Promise((resolve,reject) => {
     // Create Buffer to hold Documents
     let buffer = [];
-
-    // Create counters to use for keeping track of data progress
-    // counter is used for breaking up the documents into smaller segments
-    // lineCounter is used for bug-testing
-    // batchCounter is used to keep track of the batches being imported into DB
     let counter = 0;
     let lineCounter = 0;
-    let batchCounter = 0;
-    let data = []
+    let batchCounter = 1;
 
     // Create read stream to parse csv file
     let stream = fs.createReadStream('../Resources/photos.csv');
     stream
+      .pipe(split())
       .on("error", reject)
-      .on("data", chunk => {
+      .on("data", (line) => {
+        const data = line.toString();
+        if (counter > 1000) {
+          stream.pause()
 
-        // Parse Data into seperate arrays, removing header fields
-        var chunk = chunk.toString().split("\n")
-        if (chunk[lineCounter] === 'id, styleId, url, thumbnail_url') {
-          chunk.shift();
+          for (let i = 0; i < buffer.length; i++) {
+            if (buffer[i] === 'id, styleId, url, thumbnail_url') {
+              continue;
+            }
+            const currentEntry = buffer[i].split(",")
+            const cleanedEntry = [];
+
+            currentEntry.forEach((item) => {
+              if (item[0] === '"') {
+                item = item.substring(1, item.length);
+              }
+              if(item[item.length -1] === '"') {
+                item = item.substring(0, item.length -1);
+              }
+              cleanedEntry.push(item);
+            })
+
+            const newPhoto = new Photo({
+              id: parseInt(cleanedEntry[0]),
+              styleId: parseInt(cleanedEntry[1]),
+              url: cleanedEntry[2],
+              thumbnail_url: cleanedEntry[3],
+            })
+            .save()
+          }
+
+          buffer = [];
+          counter = 0;
+          batchCounter++
+          console.log(`Finished Batch: ${batchCounter}`);
+          stream.resume();
         }
 
-        // Iterate over every Data entry
-        chunk.forEach((entry) => {
-          lineCounter++;
-
-          // Split single string up by commas
-          let splitEntry = entry.split(",");
-          let entryArr = []
-
-          // Iterate over comma seperated string
-          splitEntry.forEach((item) => {
-
-            // Remove all double quotes from each item to clean
-            if (item[0] === '"') {
-              item = item.substring(1, item.length);
-            }
-            if (item[item.length - 1] === '"') {
-              item = item.substring(0, item.length -1);
-            }
-            // Push item to array
-            entryArr.push(item);
-          })
-
-          // if (isNaN(parseInt(entryArr[0])) || isNaN(parseInt(entryArr[1]))) {
-          //   if (fragment) {
-          //     console.log('FullLine', fragment + entry);
-          //     fragment = '';
-          //   } else {
-          //     fragment = entry;
-          //     console.log('Fragment Saved', fragment);
-          //   }
-          //   return;
-          // }
-
-          // Map item entries to Schema
-          const photoData = new Photo({
-            id: parseInt(entryArr[0]),
-            styleId: parseInt(entryArr[1]),
-            url: entryArr[2],
-            thumbnail_url: entryArr[3],
-          })
-          .save();
-
-        })
-        // stream.resume();
+        buffer.push(data);
+        lineCounter++
+        counter++;
       })
       .on("end", async () => {
+        if (buffer.length > 0) {
+          for (let i = 0; i < buffer.length; i++) {
+            if (buffer[i] === 'id, styleId, url, thumbnail_url') {
+              continue;
+            }
+            const currentEntry = buffer[i].split(",")
+            const cleanedEntry = [];
 
-        console.log('ENDED');
+            currentEntry.forEach((item) => {
+              if (item[0] === '"') {
+                item = item.substring(1, item.length);
+              }
+              if(item[item.length -1] === '"') {
+                item = item.substring(0, item.length -1);
+              }
+              cleanedEntry.push(item);
+            })
+
+            const newPhoto = new Photo({
+              id: parseInt(cleanedEntry[0]),
+              styleId: parseInt(cleanedEntry[1]),
+              url: cleanedEntry[2],
+              thumbnail_url: cleanedEntry[3],
+            }).save();
+          }
+        }
+        console.log('FINISHED ALL BATCHES');
       });
 
   })
 })()
+
--- Transformers/stylesETL.js
+++ Transformers/stylesETL.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const config = require('../config.js');
-const { Readable } = require('stream');
-const csv = require('csv-parser');
+const split = require('split');
+
+const FullStyle = require('../Models/newStyles.model');
+const Style = require('../Models/Style.model');
+const Photo = require('../Models/Photo.model');
+const Sku = require('../Models/Sku.model');
 
 (async function() {
 
@@ -14,16 +18,121 @@ const csv = require('csv-parser');
     console.log("We're connected to the server!")
   });
 
-  await new Promise((resolve,reject) => {
-    let stream = fs.createReadStream();
-    stream
-    .on("error", reject)
-    .on("data", chunk => {
+  const lineCount = 0;
 
+  await new Promise((resolve,reject) => {
+
+    let buffer = [];
+    let counter = 0;
+    let schemaCount = 0;
+
+    let stream = fs.createReadStream('../Resources/stylesSample.csv');
+    stream
+    .pipe(split())
+    .on("error", reject)
+    .on("data", (line) => {
+      const data = line.toString();
+
+      if (counter > 5) {
+        stream.pause();
+
+        for(let i = 0; i < buffer.length; i++) {
+          if (buffer[i] === 'id,productId,name,sale_price,original_price,default_style') {
+            continue;
+          }
+          const currentEntry = buffer[i].split(",")
+          const cleanedEntry = [];
+
+          currentEntry.forEach((item) => {
+            if (item[0] === '"') {
+              item = item.substring(1, item.length -1);
+            }
+            cleanedEntry.push(item);
+          })
+
+          const style_id = parseInt(cleanedEntry[0]);
+
+          Photo.find({ styleId: style_id })
+          .then((photoResults) => {
+
+            const photoData = photoResults;
+
+            Sku.find({ styleId: style_id })
+            .then((skuResults) => {
+
+            const skuData = skuResults;
+
+            const newEntry = new FullStyle({
+              product_id: parseInt(cleanedEntry[1]),
+              style_id: parseInt(cleanedEntry[0]),
+              name: parseInt(cleanedEntry[2]),
+              original_price: parseInt(cleanedEntry[4]),
+              sale_price: cleanedEntry[3],
+              default: parseInt(cleanedEntry[5]),
+              photos: photoData,
+              skus: skuData
+            }).save()
+
+            schemaCount++;
+            console.log(`Saved: ${schemaCount}`);
+            })
+            .catch((err) => console.log(err))
+          })
+          .catch((err) => console.log(err))
+        }
+
+        buffer = [];
+        counter = 0;
+        stream.resume()
+      }
+
+      counter++
+      buffer.push(data);
 
     })
     .on("end", () => {
-      console.log('done');
+      for(let i = 0; i < buffer.length; i++) {
+        if (buffer[i] === 'id,productId,name,sale_price,original_price,default_style') {
+          continue;
+        }
+
+        const currentEntry = buffer[i].split(",")
+        const cleanedEntry = [];
+
+        currentEntry.forEach((item) => {
+          if (item[0] === '"') {
+            item = item.substring(1, item.length -1);
+          }
+          cleanedEntry.push(item);
+        })
+        const style_id = parseInt(cleanedEntry[0]);
+
+        Photo.find({ styleId: style_id })
+        .then((photoResults) => {
+
+          const photoData = photoResults;
+          Sku.find({ styleId: style_id })
+            .then((skuResults) => {
+
+              const skuData = skuResults;
+
+              const newEntry = new FullStyle({
+                product_id: parseInt(cleanedEntry[1]),
+                style_id: parseInt(cleanedEntry[0]),
+                name: parseInt(cleanedEntry[2]),
+                original_price: parseInt(cleanedEntry[4]),
+                sale_price: cleanedEntry[3],
+                default: parseInt(cleanedEntry[5]),
+                photos: photoData,
+                skus: skuData
+              }).save()
+
+              console.log('saved');
+            })
+            .catch((err) => console.log(err))
+        })
+        .catch((err) => console.log(err))
+      }
     })
 
   });
